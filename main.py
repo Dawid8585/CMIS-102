cb4a711743c63bbde34c453af17c4dcaffcffa76
Dawid8585 <Perkowski8585@gmail.com>
2021-07-01 23:40:59 +0000

    1st Group assignment class 20210701 week3

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
+num = int(input("Please enter a number: \n"))
 
-while True: 
-  try:
-    
-
-    #User enters inputs of test scores
-    FirstTest  = float(input("Enter the first Test score:"))
-    SecondTest  = float(input("Enter seconf test score :"))
+if num < 15:
+    print ("{} is smaller than 15".format(num))
+else:
+    print ("x is greater than 15")
   
-    #Total is calculated
-    Total = FirstTest + SecondTest
-    
-    Avg = Total/2
-
-    #Avg grade is displayed and rounded to the 2nd decimal
-    print('\n The avg grade is:', round(Avg,2))
-    break
-    # If it fails this will be shown
-  except ValueError:
-    print("Enter a number, words are not allowed! Try again!")
-
-   
-
