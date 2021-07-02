81947dda1675a48929e1a4183a30c0451fdda110
Dawid8585 <Perkowski8585@gmail.com>
2021-07-02 01:28:12 +0000

    2nd Group assignment class 20210701

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-num = int(input("Please enter a number: \n"))
+while True: 
+  try:
+    
 
-if num < 15:
-    print ("{} is smaller than 15".format(num))
-else:
-    print ("x is greater than 15")
-  
+   
+    print("\n Ticket Prices are: \n Student Ticket: $25 \n Regular Ticket: $35 \n Senior Ticket is $20")
+
+    AgeClient  = int(input("\nEnter the age of person buying ticket:"))
+   
+    StudentTicket = 25
+    SeniorTicket = 20
+    RegularTicket = 35
+
+   
+
+    if AgeClient < 18:
+      print("Individual can not purchase ticket")
+    elif AgeClient >= 18 and AgeClient <= 25:
+      print("Student ticket price is $", StudentTicket) 
+    elif AgeClient > 60:
+      print("Senior ticket price is $", SeniorTicket)
+    elif AgeClient >= 26 and AgeClient <= 60:
+      print("Regular ticket price is $", RegularTicket)
+   
+    
+    break
+
+    # If it fails this will be shown
+  except ValueError:
+    print("Enter a number, words are not allowed! Try again!")
+
+   
+
